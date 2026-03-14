8cbe18a3cdddaad9fe8b62dc804f54bccad3d033
karishmamuskan4 <karishmamuskan248@gmail.com>
2026-03-14 12:24:06 +0530

    last deploy commit

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectDB();
 //middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true, //cokkies aur sessions allow kiye hai
   }),
 );

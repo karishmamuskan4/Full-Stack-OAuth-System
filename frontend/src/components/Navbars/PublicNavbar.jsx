5d23aa602fbc16f077ca2840c76d33809df16c49
karishmamuskan4 <karishmamuskan248@gmail.com>
2026-03-25 09:16:46 +0530

    nav btn fixed

--- frontend/src/components/Navbars/PublicNavbar.jsx
+++ frontend/src/components/Navbars/PublicNavbar.jsx
@@ -4,14 +4,19 @@ import "../../styles/nav.css";
 const PublicNavbar = () => {
   const navigate = useNavigate();
   const handleGoogleLogin = () => {
-    window.location.href = "http://localhost:3000/auth/google";
+    window.location.href = `${import.meta.env.VITE_API_URL}/auth/google`;
   };
 
   return (
     <nav className="navbar">
       <h1 className="navbar__title">OAuth Project</h1>
       <button className="navbar__google-btn" onClick={handleGoogleLogin}>
-        Continue with Google
+        <a
+          href="https://www.flaticon.com/free-icons/google"
+          title="google icons"
+        >
+          Continue with Google
+        </a>
       </button>
     </nav>
   );
